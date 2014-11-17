4d28fd72d456fa83846f7225074f799ad84c887c
gxbsst <gxbsst@gmail.com>
2014-11-17 17:33:36 +0800

    first commit

---
+++ Gemfile
@@ -0,0 +1,33 @@
+source 'http://ruby.taobao.org'
+
+gem 'rails', '4.1.7'
+gem 'rails-api'
+gem 'pg'
+
+group :development, :test do
+  gem 'rspec-rails', '~> 3.0.0'
+  gem 'factory_girl_rails'
+end
+
+group :development do
+ gem 'spring'
+ gem "spring-commands-rspec"
+end
+
+group :test do
+  gem 'database_cleaner'
+end
+# To use ActiveModel has_secure_password
+# gem 'bcrypt-ruby', '~> 3.1.2'
+
+# To use Jbuilder templates for JSON
+# gem 'jbuilder'
+
+# Use unicorn as the app server
+# gem 'unicorn'
+
+# Deploy with Capistrano
+# gem 'capistrano', :group => :development
+
+# To use debugger
+# gem 'ruby-debug19', :require => 'ruby-debug'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,19 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+
+# Although this is not needed for an api-only application, rails4 
+# requires secret_key_base or secret_token to be defined, otherwise an 
+# error is raised.
+# Using secret_token for rails3 compatibility. Change to secret_key_base
+# to avoid deprecation warning.
+# Can be safely removed in a rails3 api-only application.
+Pbl::Application.config.secret_token = '31b355f60912e50277d525b044547950e4cf080ddfb5a23a223a664b93edbf8f3693895408655662ede06883326c713a0e5b25ca64b54fe099c544c5e9c86e48'

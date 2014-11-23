6b21396dd9c824d0e147608ac19f24981b90a4d2
gxbsst <gxbsst@gmail.com>
2014-11-23 17:50:42 +0800

    添加密码验证服务。

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 
 class User < ActiveRecord::Base
 
-  # validates_confirmation_of :password
-  # validates :email, :presence => true, :uniqueness => true, :email_format => true
+  validates_confirmation_of :password
+  validates :email, :presence => true, :uniqueness => true, :email_format => true
 
   include Authentication
 

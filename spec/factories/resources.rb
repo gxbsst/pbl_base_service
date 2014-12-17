0c857b761618c94f4f00d597c8077bb53a166bb8
gxbsst <gxbsst@gmail.com>
2014-12-17 16:16:52 +0800

    创建资源库。

--- spec/factories/resources.rb
+++ spec/factories/resources.rb
@@ -1,6 +1,18 @@
 FactoryGirl.define do
   factory :resource do
-    
+   name 'name'
+   owner_id ''
+   owner_type ''
+   size 'size'
+   ext 'ext'
+   mime_type 'mime_type'
+   md5 'md5'
+   key 'key'
+   exif 'exif'
+   image_info 'image_info'
+   image_ave 'image_ave'
+   persistent_id 'persistent_id'
+   avinfo 'avinfo'
   end
 
 end
--- spec/requests/v1/resources_controller_spec.rb
+++ spec/requests/v1/resources_controller_spec.rb
@@ -34,30 +34,30 @@ describe V1::ResourcesController do
     context 'with found' do
       let!(:resource)  { create :resource, owner_type: owner.class.name, owner_id: owner.id, name: 'name 1' }
       before(:each) do
-        get "/resources/#{resources.id}", {}, accept
+        get "/resources/#{resource.id}", {}, accept
         @json = parse_json(response.body)
       end
 
-      it { expect(@json['id']).to eq(gauge.id.to_s) }
+      it { expect(@json['id']).to eq(resource.id.to_s) }
       it { expect(@json['name']).to eq('name 1') }
-      it { expect(@json['owner_id']).to eq('level_2') }
-      it { expect(@json['owner_type']).to eq('level_3') }
-      it { expect(@json['size']).to eq('level_4') }
-      it { expect(@json['ext']).to eq('level_5') }
-      it { expect(@json['mime_type']).to eq('level_6') }
-      it { expect(@json['md5']).to eq('level_7') }
+      it { expect(@json['owner_id']).to_not be_nil }
+      it { expect(@json['owner_type']).to eq(owner.class.name) }
+      it { expect(@json['size']).to eq('size') }
+      it { expect(@json['ext']).to eq('ext') }
+      it { expect(@json['mime_type']).to eq('mime_type') }
+      it { expect(@json['md5']).to eq('md5') }
       it { expect(@json['key']).to eq('key') }
-      it { expect(@json['exif']).to eq('key') }
-      it { expect(@json['image_info']).to eq('image_infoy') }
+      it { expect(@json['exif']).to eq('exif') }
+      it { expect(@json['image_info']).to eq('image_info') }
       it { expect(@json['image_ave']).to eq('image_ave') }
       it { expect(@json['persistent_id']).to eq('persistent_id') }
       it { expect(@json['avinfo']).to eq('avinfo') }
     end
 
     context 'with not found' do
-      let!(:gauge) { create :gauge, technique_id: technique.id }
+      let!(:resource)  { create :resource, owner_type: owner.class.name, owner_id: owner.id, name: 'name 1' }
       before(:each) do
-        get '/pbl/products/16720e7f-74d4-4c8f-afda-9657e659b432', {}, accept
+        get '/resources/16720e7f-74d4-4c8f-afda-9657e659b432', {}, accept
       end
 
       it { expect(response.status).to  eq(404) }
@@ -67,28 +67,16 @@ describe V1::ResourcesController do
   describe 'POST #create' do
     context 'with successful' do
       before(:each) do
-        post '/gauges', { gauge: attributes_for(:gauge, technique_id: technique.id) }, accept
+        post '/resources', { resource: attributes_for(:resource, owner_id: owner.id, owner_type: owner.class.name) }, accept
         @json = parse_json(response.body)
       end
 
-      it { expect(@json['level_1']).to eq('level_1') }
-    end
-  end
-
-  describe 'PATCH #update' do
-    context 'with successful' do
-      let!(:gauge) { create :gauge, technique_id: technique.id }
-      before(:each) do
-        patch "/gauges/#{gauge.id}", {gauge: attributes_for(:gauge, level_1: 'level_1', technique_id: technique.id)}, accept
-        @json = parse_json(response.body)
-      end
-
-      it { expect(@json['id']).to eq(gauge.id) }
+      it { expect(@json['name']).to eq('name') }
     end
   end
 
   describe 'DELETE #destroy' do
-    let!(:gauge) { create :gauge, technique_id: technique.id }
-    it { expect{  delete "/gauges/#{gauge.id}", {}, accept }.to change(Gauge, :count).from(1).to(0) }
+    let!(:resource)  { create :resource, owner_type: owner.class.name, owner_id: owner.id, name: 'name 1' }
+    it { expect{  delete "/resources/#{resource.id}", {}, accept }.to change(Resource, :count).from(1).to(0) }
   end
 end
